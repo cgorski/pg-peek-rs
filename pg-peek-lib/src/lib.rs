01e53df4461b960065c9f8248953038a28479e2b
Chris Gorski <cgorski@cgorski.org>
2023-10-05 14:04:15 -0400

    tupledata

--- pg-peek-lib/src/lib.rs
+++ pg-peek-lib/src/lib.rs
@@ -1,5 +1,5 @@
-use bitflags::{bitflags, Flags};
-use byteorder::{ReadBytesExt};
+use bitflags::Flags;
+use byteorder::ReadBytesExt;
 
 use std::io::{self, Read};
 
@@ -76,7 +76,7 @@ pub struct PageHeaderData {
 // ItemIdData structure
 
 bitflags! {
-    #[derive(Debug)]
+    #[derive(Debug, PartialEq)]
     pub struct LPFlags: u8 {
         const LP_UNUSED   = 0x00;
         const LP_NORMAL   = 0x01;
@@ -111,17 +111,17 @@ impl ItemIdData {
 }
 
 // HeapTupleHeaderData structure
-#[derive(Debug)]
-pub struct HeapTupleHeaderData {
-    t_xmin: TransactionId,
-    t_xmax: TransactionId,
-    t_cid: CommandId,
-    t_xvac: TransactionId,
-    t_ctid: ItemPointerData,
-    t_infomask2: u16,
-    t_infomask: u16,
-    t_hoff: u8,
-}
+// #[derive(Debug)]
+// pub struct HeapTupleHeaderData {
+//     t_xmin: TransactionId,
+//     t_xmax: TransactionId,
+//     t_cid: CommandId,
+//     t_xvac: TransactionId,
+//     t_ctid: ItemPointerData,
+//     t_infomask2: u16,
+//     t_infomask: u16,
+//     t_hoff: u8,
+// }
 
 // Varlena structure
 #[derive(Debug)]
@@ -159,7 +159,7 @@ pub struct PageLayout {
     header: PageHeaderData,
     item_identifiers: Vec<ItemIdData>,
     free_space: Vec<u8>,
-    items: Vec<Varlena>,
+    items: Vec<HeapTuple>,
     special_space: Option<SpecialSection>,
 }
 
@@ -219,7 +219,6 @@ pub fn read_item_identifiers<R: Read>(
 }
 
 pub const DEFAULT_POSTGRES_PAGE_SIZE: usize = 8192; // Default Postgres page size in bytes
-
 pub fn read_all_pages<R: Read>(
     reader: &mut R,
     endianness: Endianness,
@@ -238,20 +237,141 @@ pub fn read_all_pages<R: Read>(
             ));
         }
 
-        let mut cursor = io::Cursor::new(buffer);
+        let mut cursor = io::Cursor::new(&buffer);
         let header = read_page_header(&mut cursor, endianness)?;
         let item_identifiers = read_item_identifiers(&mut cursor, &header, endianness)?;
+
+        // Read HeapTuples for each item identifier
+        let mut items = Vec::new();
+        for item_id in &item_identifiers {
+            if item_id.lp_flags == LPFlags::LP_NORMAL {
+                let tuple_length = item_id.lp_len as u32; // Assuming lp_len is the length including the header
+                let tuple = HeapTuple::from_reader(&mut cursor, tuple_length, endianness)?;
+                items.push(tuple);
+            } else {
+                // Skip non-NORMAL items based on their length
+                cursor.set_position(cursor.position() + item_id.lp_len as u64);
+            }
+        }
+
+        // We will calculate the free space based on where the cursor is positioned
+        let remaining_bytes = DEFAULT_POSTGRES_PAGE_SIZE as u64 - cursor.position();
+        let mut free_space = vec![0u8; remaining_bytes as usize];
+        cursor.read_exact(&mut free_space)?;
 
         let page_layout = PageLayout {
             header,
             item_identifiers,
-            free_space: Vec::new(),
-            items: Vec::new(),
-            special_space: None,
+            free_space,
+            items,
+            special_space: None, // Placeholder, adjust as needed
         };
 
         pages.push(page_layout);
     }
 
     Ok(pages)
+}
+
+use bitflags::bitflags;
+
+bitflags! {
+    #[derive(Debug)]
+    struct Infomask2: u16 {
+        const HEAP_NATTS_MASK = 0x07FF;
+        const HEAP_KEYS_UPDATED = 0x2000;
+        const HEAP_HOT_UPDATED = 0x4000;
+        const HEAP_ONLY_TUPLE = 0x8000;
+        const HEAP2_XACT_MASK = 0xE000;
+    }
+}
+
+bitflags! {
+    #[derive(Debug)]
+    struct Infomask: u16 {
+        const HEAP_HASNULL = 0x0001;
+        const HEAP_HASVARWIDTH = 0x0002;
+        const HEAP_HASEXTERNAL = 0x0004;
+        const HEAP_HASOID_OLD = 0x0008;
+        const HEAP_XMAX_KEYSHR_LOCK = 0x0010;
+        const HEAP_COMBOCID = 0x0020;
+        const HEAP_XMAX_EXCL_LOCK = 0x0040;
+        const HEAP_XMAX_LOCK_ONLY = 0x0080;
+        const HEAP_XMAX_SHR_LOCK = Self::HEAP_XMAX_EXCL_LOCK.bits() | Self::HEAP_XMAX_KEYSHR_LOCK.bits();
+        const HEAP_LOCK_MASK = Self::HEAP_XMAX_SHR_LOCK.bits() | Self::HEAP_XMAX_EXCL_LOCK.bits() | Self::HEAP_XMAX_KEYSHR_LOCK.bits();
+        const HEAP_XMIN_COMMITTED = 0x0100;
+        const HEAP_XMIN_INVALID = 0x0200;
+        const HEAP_XMIN_FROZEN = Self::HEAP_XMIN_COMMITTED.bits() | Self::HEAP_XMIN_INVALID.bits();
+        const HEAP_XMAX_COMMITTED = 0x0400;
+        const HEAP_XMAX_INVALID = 0x0800;
+        const HEAP_XMAX_IS_MULTI = 0x1000;
+        const HEAP_UPDATED = 0x2000;
+        const HEAP_MOVED_OFF = 0x4000;
+        const HEAP_MOVED_IN = 0x8000;
+        const HEAP_MOVED = Self::HEAP_MOVED_OFF.bits() | Self::HEAP_MOVED_IN.bits();
+        const HEAP_XACT_MASK = 0xFFF0;
+    }
+}
+
+#[derive(Debug)]
+pub struct HeapTuple {
+    header: HeapTupleHeaderData,
+    data: Vec<u8>,
+}
+
+impl HeapTuple {
+    pub fn from_reader<R: Read>(
+        reader: &mut R,
+        total_length: u32,
+        endianness: Endianness,
+    ) -> io::Result<HeapTuple> {
+        let header = HeapTupleHeaderData::read_from(reader, endianness)?;
+
+        // Calculate the size of data by subtracting the size of the header from the total length.
+        let data_length = total_length as usize - std::mem::size_of::<HeapTupleHeaderData>();
+        let mut data = vec![0u8; data_length];
+        reader.read_exact(&mut data)?;
+
+        Ok(HeapTuple { header, data })
+    }
+}
+
+#[derive(Debug)]
+pub struct HeapTupleHeaderData {
+    t_xmin: TransactionId,
+    t_xmax: TransactionId,
+    t_cid: CommandId,
+    t_ctid: ItemPointerData,
+    t_infomask2: Infomask2,
+    t_infomask: Infomask,
+    t_hoff: u8,
+}
+
+impl HeapTupleHeaderData {
+    pub fn read_from<R: Read>(
+        reader: &mut R,
+        endianness: Endianness,
+    ) -> io::Result<HeapTupleHeaderData> {
+        let t_xmin = TransactionId(read_u32(reader, endianness)?);
+        let t_xmax = TransactionId(read_u32(reader, endianness)?);
+        let t_cid = CommandId(read_u32(reader, endianness)?); // same as t_xvac
+        let t_ctid = {
+            let mut buffer = [0u8; 6];
+            reader.read_exact(&mut buffer)?;
+            ItemPointerData(buffer)
+        };
+        let t_infomask2 = Infomask2::from_bits_truncate(read_u16(reader, endianness)?);
+        let t_infomask = Infomask::from_bits_truncate(read_u16(reader, endianness)?);
+        let t_hoff = reader.read_u8()?;
+
+        Ok(HeapTupleHeaderData {
+            t_xmin,
+            t_xmax,
+            t_cid,
+            t_ctid,
+            t_infomask2,
+            t_infomask,
+            t_hoff,
+        })
+    }
 }
